8a2a679df227fd44573de2771b236c412b383262
Regio Matheus <regio.pimentel2016@gmail.com>
2024-01-29 20:42:01 -0300

    inicia projeto

---
+++ src/main/java/com/wakacop/wakacop/WakacopApplication.java
@@ -0,0 +1,13 @@
+package com.wakacop.wakacop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WakacopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WakacopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wakacop/wakacop/WakacopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wakacop.wakacop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WakacopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
