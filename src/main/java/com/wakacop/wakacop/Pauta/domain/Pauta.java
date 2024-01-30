8135a7e35b0cd4c79359665c65c01d791074b28b
Regio Matheus <regio.pimentel2016@gmail.com>
2024-01-30 10:43:18 -0300

    Implementa camada de dominio

---
+++ src/main/java/com/wakacop/wakacop/Pauta/domain/Pauta.java
@@ -0,0 +1,24 @@
+package com.wakacop.wakacop.Pauta.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@ToString
+@Entity
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Pauta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(columnDefinition = "uuid", updatable = false,unique = true,nullable = false)
+    private UUID id;
+    private String titulo;
+    private String descricao;
+    private UUID idAssociadoAutor;
+    private LocalDateTime dataCriacao;
+}
