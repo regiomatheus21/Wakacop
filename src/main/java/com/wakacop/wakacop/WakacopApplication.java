de135342db4676b161874240a79ff9b9d4fb0d0a
Regio Matheus <regio.pimentel2016@gmail.com>
2024-02-14 12:05:33 -0300

    Finaliza Ajustes

--- src/main/java/com/wakacop/wakacop/WakacopApplication.java
+++ src/main/java/com/wakacop/wakacop/WakacopApplication.java
@@ -3,6 +3,7 @@ package com.wakacop.wakacop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/")
 @EnableScheduling
 public class WakacopApplication {
+
+	@GetMapping
+	public String testeHome(){
+		return "teste regio wakacop";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WakacopApplication.class, args);
