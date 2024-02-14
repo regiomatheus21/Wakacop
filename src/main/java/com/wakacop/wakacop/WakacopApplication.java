671e60477125eb8b5a06b1e039c06653688af668
Regio Matheus <regio.pimentel2016@gmail.com>
2024-02-14 11:24:55 -0300

    implementa finalizador de sessao

--- src/main/java/com/wakacop/wakacop/WakacopApplication.java
+++ src/main/java/com/wakacop/wakacop/WakacopApplication.java
@@ -2,12 +2,14 @@ package com.wakacop.wakacop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@RequestMapping("/")
+@RequestMapping(value = "/")
+@EnableScheduling
 public class WakacopApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/wakacop/wakacop/sessaovotacao/application/service/SessaoVotacaoRepository.java
+++ src/main/java/com/wakacop/wakacop/sessaovotacao/application/service/SessaoVotacaoRepository.java
@@ -2,6 +2,7 @@ package com.wakacop.wakacop.sessaovotacao.application.service;
 
 import com.wakacop.wakacop.sessaovotacao.domain.SessaoVotacao;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface SessaoVotacaoRepository {
@@ -9,4 +10,6 @@ public interface SessaoVotacaoRepository {
     SessaoVotacao salva(SessaoVotacao sessaoVotacao);
 
     SessaoVotacao buscaPorId(UUID idSessao);
+
+    List<SessaoVotacao> buscaAbertas();
 }
