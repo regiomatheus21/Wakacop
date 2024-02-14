0183aee17653d8dde503f9a02fa308e334bf3b0e
Regio Matheus <regio.pimentel2016@gmail.com>
2024-02-14 10:46:06 -0300

    Finaliza Associado

---
+++ src/main/java/com/wakacop/wakacop/associado/infra/client/ConsultaCPFResponse.java
@@ -0,0 +1,34 @@
+package com.wakacop.wakacop.associado.infra.client;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.ToString;
+@Getter
+@ToString
+public class ConsultaCPFResponse {
+    @JsonProperty("ni")
+    private String ni;
+    @JsonProperty("nome")
+    private String nome;
+    @JsonProperty("situacao")
+    private Situacao situacao;
+    private  String nascimento;
+
+    public boolean isInvalid() {
+        return !isValid();
+    }
+
+    public boolean isValid() {
+        return this.situacao.getCodigo().equals("0");
+    }
+
+
+    @Getter
+    @ToString
+    public static class Situacao{
+        @JsonProperty("codigo")
+        private String codigo;
+        @JsonProperty("descricao")
+        private String descricao;
+    }
+}
